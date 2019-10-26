8a0d48dfe59cb3b7db1530dc10e8954df7ac331e
Jon Craton <jncraton@gmail.com>
2019-10-26 09:15:52 -0400

    Add MPI boilerplate

--- src/pi.c
+++ src/pi.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <mpi.h>
 #define INTERVALS 1000000
 
-int main(int arc, char* argv[])
+int main(int argc, char* argv[])
 {
+  MPI_Init (&argc, &argv);
+
+  int process_rank, num_processes;
+  MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+  MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  double elapsed_time = -MPI_Wtime();
+
 	double area;	/* The final anser */
 	double ysum;	/* Sum of rectangle heights */
 	double xi;	/* Midpoint of interval */
@@ -16,7 +26,16 @@ int main(int arc, char* argv[])
 		ysum+=4.0/(1.0+xi*xi);
 	}
 
-	area = ysum * (1.0/INTERVALS);
-	printf("pi is %13.11f\n", area);
-	return 0;
+  elapsed_time += MPI_Wtime();
+
+  MPI_Finalize();
+
+  if (process_rank == 0) {
+  	area = ysum * (1.0/INTERVALS);
+  	printf("pi is %13.11f\n", area);
+    printf("Calculated in %f\n", elapsed_time);
+    fflush (stdout);
+  }
+  
+  return 0;
 }
